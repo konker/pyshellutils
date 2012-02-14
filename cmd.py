cbb77f3d9fe117bd8258c9cfede0122aa44d72e1
konker.morningwoodsvr1 <konker@gmail.com>
2012-02-14 08:48:45 -0500

    Initial commit

---
+++ cmd.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# shell.cmd
+# 
+# Execute shell commands in a subprocess
+#
+# Authors: Konrad Markus <konker@gmail.com>
+#
+
+import subprocess
+
+def exec_cmd_out(cmd, cwd=None):
+    stdout,stderr = exec_cmd(cmd, cwd)
+    return stdout
+
+def exec_cmd_err(cmd, cwd=None):
+    stdout,stderr = exec_cmd(cmd, cwd)
+    return stderr
+
+def exec_cmd(cmd, cwd=None):
+    pipe = subprocess.Popen(cmd, shell=True, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout,stderr = pipe.communicate()
+    pipe.wait()
+    return stdout,stderr
+
